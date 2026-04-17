1023919f91b0c5982db0b2b65bc81ae1476b215e
Nitin Patil <ni3patil2601@gmail.com>
2026-04-17 14:13:17 +0530

    Day 9 of Learning Python

---
+++ Day9/17_ForLoop.py
@@ -0,0 +1,23 @@
+#A for loop is used to iterate over a sequence (list, tuple, string, set, dict).
+#Syntax:     for variable in iterable:
+                # code
+
+# str = "nitin"
+# for ch in str:
+#     print(ch)
+
+# range() (most important)
+# range(n) → 0 to n-1
+# range(start, end) → start to end-1
+# range(start, end, step) → with step
+
+# for i in range (5):
+#     print(i)
+
+# for j in range (1,5):
+#     print(j)
+
+for k in range(0,10,3):
+    print(k)
+
+#Pass → does nothing
---
+++ Day9/PraQue18.py
@@ -0,0 +1,24 @@
+# Print the element in following list using for loop
+
+# numb = [1,4,9,16,25,36,64,81,100]
+# for value in numb:
+#     print(value)
+
+#search for number x in following tupple 
+tup = (1,4,9,16,25,36,64,81,100)
+# x = int(input("Enter number to search: "))
+# idx=0
+# for value in tup:
+#     idx += 1
+#     if value == x:        
+#         print("Number found at index: ",idx)
+#         break
+# else :
+#     print("Number not found")    
+
+# for i in range (1,11):
+#     print(i)
+
+# for j in range(10,0,-1):
+#     print(j)    
+
