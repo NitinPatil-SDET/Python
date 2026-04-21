98bdfdffc6236c6ffc29c2b179b46621b7a9917f
Nitin Patil <ni3patil2601@gmail.com>
2026-04-21 11:44:50 +0530

    Day 11 of Learning of Python

--- Day11/PraQue24.py
+++ Day11/PraQue24.py
@@ -6,6 +6,14 @@ path = "Day11/Practice.txt"
 
 with open(path, "r") as f:
     data = f.read()
+    print(data)
 
-print(data)
+new_data = data.replace("Java", "Python")
+
+with open(path, "w+") as f:
+    f.write(new_data)
+    print(f.read())
+
+    
+    
 
---
+++ Day11/PraQue25.py
@@ -0,0 +1,24 @@
+#WAF to find in which line of the file does the word "learning" occurec first.
+#Print -1 if word not found
+
+path = "Day11/Practice.txt"
+word = "learning"
+def find_word(word):
+    with open(path, "r") as f:
+        line_no = 1
+        found = False
+        for line in f :
+            if word in line:
+                print(line_no)
+                found = True
+                break
+            line_no += 1
+        if not found:
+            print(-1)
+
+find_word(word) #1
+find_word("AI") #2
+find_word("Nitin") #-1
+            
+
+
