7a54c752320ef1778c076f9b48d95e448266c48c
Nitin Patil <ni3patil2601@gmail.com>
2026-04-23 14:18:23 +0530

    Day 13 of Leaning Python

--- Day13/24_Keyword.py
+++ Day13/24_Keyword.py
@@ -37,79 +37,24 @@
 # This way, the child class can leverage the functionality of the parent class.
 
 # Parent Class: Animal
-# class Animal:
-#     def __init__(self, name):
-#         self.name = name
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
-#     def info(self):
-#         print("Animal name:", self.name)
+    def info(self):
+        print("Animal name:", self.name)
 
 # # Child Class: Dog
-# class Dog(Animal):
-#     def __init__(self, name, breed):
-#         super().__init__(name)   # Call parent constructor
-#         self.breed = breed
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)   # Call parent constructor
+        self.breed = breed
 
-#     def details(self):
-#         print(self.name, "is a", self.breed)
+    def details(self):
+        print(self.name, "is a", self.breed)
 
 # d = Dog("Buddy", "Golden Retriever")
 # d.info()      # Parent method
 # d.details()   # Child method
-
-
-#--------------------------------------------------------------------------------------------------------------------------------------------
-#class method --> A class method is bound to the class & recive the class an 
-
-# 🔹 1. Instance Method
-# Works with object data (self)
-# Can access & modify instance attributes
-# Instance method → works on object (self) data
-
-class Student:
-    def __init__(self, name):
-        self.name = name
-
-    def change_name(self, new_name):   # instance method
-        self.name = new_name
-
-s = Student("A")
-print(s.change_name("B"))
-
-
-
-# 🔹 2. Class Method
-# Works with class data (cls)
-# Can access/modify class variables
-# Class method → works on class (shared) data
-
-class Student:
-    school = "ABC"
-
-    @classmethod
-    def change_school(cls, new_name):
-        cls.school = new_name
-
-print(Student.change_school("XYZ"))
-
-# 🔹 3. Static Method
-# No access to self or cls
-# Just a utility/helper function inside class
-# Static method → no relation to object or class data
-class Student:
-    @staticmethod
-    def greet():
-        print("Hello")
-
-print(Student.greet())
-
-
-
-
-# | Type            | Uses `self` | Uses `cls` | Purpose               |
-# | --------------- | ----------- | ---------- | --------------------- |
-# | Instance Method | ✅           | ❌          | Work with object data |
-# | Class Method    | ❌           | ✅          | Work with class data  |
-# | Static Method   | ❌           | ❌          | Utility function      |
 
 
